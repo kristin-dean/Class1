2b1da414a4ed5bd3210fc849dddb2425f6740ca8
kristin-dean <47224694+kristin-dean@users.noreply.github.com>
2019-03-06 19:57:04 -0500

    Update makeCharts.js

--- makeCharts.js
+++ makeCharts.js
@@ -5,6 +5,7 @@ var dataP = d3.csv("prostate.csv");
 dataB.then(function(data)
 {
   console.log("data",data);
+  drawChart(data, "#breast");
 },
 function(err)
 {
@@ -14,6 +15,7 @@ function(err)
 dataP.then(function(dataName)
 {
   console.log("data",dataName);
+    drawChart(dataName, "#prostate");
 },
 function(err)
 {
@@ -23,3 +25,24 @@ function(err)
 
 //************************************** function to create charts ******************************************//
 
+var drawChart = function(colorData, idname)
+{
+  var width = 800;
+  var height = 200;
+  var barWidth = width/colorData.length - 20;
+  var svg = d3.select(idname)
+              .attr("height", height)
+              .attr("width", width);
+  svg.selectAll("rect")
+     .data(colorData)
+     .enter()
+     .append("rect")
+     .attr("x", function(d,i)
+      { return i*barWidth;})
+    .attr("y", function (d)
+      { return height - d.Number/15;})
+    .attr("width", barWidth)
+    .attr("height", function(d)
+      { return d.Number/15;})
+    .attr("fill", function(d)
+      { return "pink"})
