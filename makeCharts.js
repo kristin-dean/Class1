ac6a3e2e9cb69efde07641608dcf9995ae1903b9
kristin-dean <47224694+kristin-dean@users.noreply.github.com>
2019-03-06 20:56:47 -0500

    Update makeCharts.js

--- makeCharts.js
+++ makeCharts.js
@@ -59,6 +59,6 @@ var drawChart = function(colorData, idname, color)
    .attr("fill", "black")
    .attr("font-family", "sans-serif")
    .attr("font-size", "11px")
-   .attr("transform", "rotate(90deg)")
+   .attr("transform", "rotate(90deg)");
 
 }
