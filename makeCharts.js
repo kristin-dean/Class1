e2018a873ad7b70bbf4e41b24971ce8182212c07
kristin-dean <47224694+kristin-dean@users.noreply.github.com>
2019-03-06 19:48:00 -0500

    Update makeCharts.js

--- makeCharts.js
+++ makeCharts.js
@@ -1,1 +1,29 @@
 
+var dataB = d3.csv("breast.csv");
+var dataP = d3.csv("prostate.csv");
+
+dataP.then(function(data)
+{
+  console.log("data",data);
+  drawChart(data, "#JSON");
+  drawLegend(data, "#JSONkey");
+},
+function(err)
+{
+  console.log(err);
+});
+
+dataG.then(function(dataName)
+{
+  console.log("data",dataName);
+  drawChart(dataName, "#CSV");
+  drawLegend(dataName, "#CSVkey");
+},
+function(err)
+{
+  console.log(err);
+});
+
+
+//************************************** function to create charts ******************************************//
+
